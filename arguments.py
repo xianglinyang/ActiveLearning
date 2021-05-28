1b4881f63554e285ad47354df6da343a665262e4
xianglin <yxlyzxl@gmail.com>
2021-05-28 18:36:12 +0800

    add task model type argument

--- arguments.py
+++ arguments.py
@@ -11,6 +11,7 @@ def get_arguments():
     parser.add_argument('--dataset', type=str, default="CIFAR10", required=True,help='dataset for training',
                         choices=["CIFAR10", "MNIST", "CIFAR100", "FashionMNIST", "SVHN"])
     parser.add_argument('--save', '-s', type=bool, default=True, required=True, help='save datasets and task models')
+    parser.add_argument('--task_model_type', type=str, default="pytorch", choices=["tensorflow", "pytorch"])
     args = parser.parse_args()
 
     return args
