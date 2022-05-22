2d51d1763babaaed7b9b4071945ce964cb589270
Yang Xianglin <yxlyzxl@gmail.com>
2022-05-22 19:50:11 +0800

    refactor, fix bug

---
+++ Run/utils.py
@@ -0,0 +1,108 @@
+import os
+from sklearn.utils import shuffle
+import torch
+import torchvision
+import json
+
+
+def save_datasets(strategy_n, model_n, dataset_n, gpu=None, **kwargs):
+    # strategy path
+    strategy_n_path = os.path.join("..", "..", "..", "DVI_data", "active_learning", strategy_n)
+    # strategy path
+    os.system("mkdir -p {}".format(strategy_n_path))
+    # task model path
+    model_n_path = os.path.join(strategy_n_path, model_n)
+    os.system("mkdir -p {}".format(model_n_path))
+    # dataset path
+    dataset_n_path = os.path.join(model_n_path, dataset_n)
+    training_path = os.path.join(dataset_n_path, "Training_data")
+    testing_path = os.path.join(dataset_n_path, "Testing_data")
+    os.system("mkdir -p {}".format(training_path))
+    os.system("mkdir -p {}".format(testing_path))
+    # Model dir
+    model_path = os.path.join(dataset_n_path, "Model")
+    os.system("mkdir -p {}".format(model_path))
+
+    # save dataset
+    # device = torch.device("cuda:0" if args.cuda and torch.cuda.is_available() else "cpu")
+    if gpu is None:
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cuda:{}".format(gpu))
+    if dataset_n == "CIFAR10":
+        train_dataset = torchvision.datasets.CIFAR10(root='..//data//CIFAR10', download=True,
+                                                     transform=kwargs['transform_te'], train=True)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle=False, batch_size=500)
+        test_dataset = torchvision.datasets.CIFAR10(root='..//data//CIFAR10', download=True,
+                                                    transform=kwargs['transform_te'], train=False)
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle=False, batch_size=500)
+    elif dataset_n == "SVHN":
+        # TODO
+        pass
+    elif dataset_n == "CIFAR100":
+        # TODO
+        pass
+    elif dataset_n == "MNIST":
+        # TODO
+        pass
+    training_data = torch.Tensor().to(device)
+    training_labels = torch.Tensor().to(device)
+    for data, target in train_dataloader:
+        data = data.to(device)
+        target = target.to(device)
+        training_data = torch.cat((training_data, data), 0)
+        training_labels = torch.cat((training_labels, target), 0)
+    training_data_path = os.path.join(training_path, "training_dataset_data.pth")
+    training_labels_path = os.path.join(training_path, "training_dataset_label.pth")
+    torch.save(training_data, training_data_path)
+    torch.save(training_labels, training_labels_path)
+
+
+    testing_data = torch.Tensor().to(device)
+    testing_labels = torch.Tensor().to(device)
+    for data, target in test_dataloader:
+        data = data.to(device)
+        target = target.to(device)
+        testing_data = torch.cat((testing_data, data), 0)
+        testing_labels = torch.cat((testing_labels, target), 0)
+    testing_data_path = os.path.join(testing_path, "testing_dataset_data.pth")
+    testing_labels_path = os.path.join(testing_path, "testing_dataset_label.pth")
+    torch.save(testing_data, testing_data_path)
+    torch.save(testing_labels, testing_labels_path)
+
+
+def save_task_model(n_epoch, strategy):
+    # save subject model and index
+    working_path = os.path.join("..", "..", "..", "DVI_data", "active_learning", strategy.strategy_name, strategy.model_name, strategy.dataset_name, "Model")
+    if not os.path.exists(working_path):
+        os.mkdir(working_path)
+    working_path = os.path.join(working_path, "Iteration_{}".format(n_epoch))
+    if not os.path.exists(working_path):
+        os.mkdir(working_path)
+    task_model_path = os.path.join(working_path, "subject_model.pth")
+    torch.save(strategy.task_model.state_dict(), task_model_path)
+
+    current_indices = strategy.lb_idxs.tolist()
+    with open(os.path.join(working_path, "index.json"), "w") as f:
+        json.dump(current_indices, f)
+
+
+def save_model(n_epoch, strategy, model, name):
+    # save subject model and index
+    working_path = os.path.join("..", "..", "..", "DVI_data", "active_learning", strategy.strategy_name, strategy.model_name, strategy.dataset_name, "Model")
+    if not os.path.exists(working_path):
+        os.mkdir(working_path)
+    working_path = os.path.join(working_path, "Iteration_{}".format(n_epoch))
+    if not os.path.exists(working_path):
+        os.mkdir(working_path)
+    model_path = os.path.join(working_path, name+".pth")
+    torch.save(model.state_dict(), model_path)
+
+
+def save_new_select(iteration, strategy, new_selected):
+    working_path = os.path.join("..", "..", "..", "DVI_data", "active_learning", strategy.strategy_name, strategy.model_name, strategy.dataset_name, "Model", "Iteration_{}".format(iteration))
+    os.system("mkdir -p {}".format(working_path))
+    new_selection = new_selected.tolist()
+    with open(os.path.join(working_path, "new_selected.json"), "w") as f:
+        json.dump(new_selection, f)
+
--- arguments.py
+++ arguments.py
@@ -4,7 +4,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description="Get arguments for active learning.")
 
     parser.add_argument('--method', '-m', type=str, default="random", choices=['random', 'LeastConfidence', 'coreset', "LL4AL"],
-                        required=True, help="active learning query strategy")
+                        help="active learning query strategy")
     parser.add_argument('--init_num', type=int, default=1000, help="init label number")
     parser.add_argument('--query_num', type=int, default=1000, help="query budget for each cycle")
     parser.add_argument('--cycle_num', type=int, default=10, help="query cycle number")
@@ -13,8 +13,9 @@ def get_arguments():
                         choices=["CIFAR10", "MNIST", "CIFAR100", "FashionMNIST", "SVHN"])
     parser.add_argument('--save', '-s', type=bool, default=True, help='save datasets and task models')
     parser.add_argument('--task_model_type', type=str, default="pytorch", choices=["tensorflow", "pytorch"])
-    parser.add_argument("--resume", '-r', type=bool, default=False, help="resume from checkpoints")
-    parser.add_argument("--resume_path", type=str, help="path to where ckpt is saved")
+    parser.add_argument("--gpu",'-g', default="0", choices=[None, "0", "1", "2", "3"])
+    parser.add_argument("--resume", '-r', type=int, default=1, help="resume from checkpoints")
+    parser.add_argument("--resume_path", default="/home/xianglin/projects/DVI_data/active_learning/base/resnet18/CIFAR10/Model/Iteration_1/", type=str, help="path to where ckpt is saved")
     args = parser.parse_args()
 
     return args
